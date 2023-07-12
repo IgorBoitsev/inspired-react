48aeca4d306e8b5f9acd2e99da50498fc342c7cb
Igor Boitsev <igor.boitsev@gmail.com>
2023-07-12 08:12:12 +0300

    Top & Container styles

--- src/Components/Header/Top/Top.jsx
+++ src/Components/Header/Top/Top.jsx
@@ -1,7 +1,12 @@
+import { Container } from "../../Layout/Container/Container";
+import style from './Top.module.scss';
+
 export const Top = () => (
     <div className="top">
-        <p>Телефон</p>
-        <p>Логотип</p>
-        <p>Иконочки</p>
+        <Container>
+            <p>Телефон</p>
+            <p>Логотип</p>
+            <p>Иконочки</p>
+        </Container>
     </div>
 )
